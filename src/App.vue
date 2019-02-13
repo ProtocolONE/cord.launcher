80a7e2ffb71b464c4cc881c0bac33727c416a4c0
borisbutenko <iamborisbutenko@gmail.com>
2019-02-13 15:58:37 +0400

    add sentry bugs tracker

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,23 @@
 <script>
 import AppLayout from '@layouts/app/AppLayout'
 
+if (process.env.NODE_ENV === 'development') { // production
+  const Vue = require('vue').default
+  const Raven = require('raven-js')
+  const RavenVue = require('raven-js/plugins/vue')
+
+  try {
+    Raven
+      .config('https://19b75f3e25324623991e29c1d87da0cd@sentry.tst.protocol.one/8')
+      .addPlugin(RavenVue, Vue)
+      .install()
+    console.log('Raven is successfully installed.')
+  }
+  catch (error) {
+    console.error(`Raven is dropped with error: ${ error }`)
+  }
+}
+
 // --- TODO: вынести куда-то в более адекватное место
 function calculateViewportHeight (percent = 0.01) {
   let vh = (window.innerHeight * percent)
