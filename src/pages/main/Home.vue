45f59e56bfb4b726764bb3a4bc177aad4613aa4e
borisbutenko <iamborisbutenko@gmail.com>
2019-03-18 13:13:53 +0400

    v0.0.1

--- src/pages/main/Home.vue
+++ src/pages/main/Home.vue
@@ -1,19 +1,13 @@
 <template>
 <article id="home" class="cord-padding">
-  <h1 class="text-center">{{ message }}</h1>
+  <h1 class="text-center">
+    Home
+  </h1>
 </article>
 </template>
 
 <script>
-import { remote } from 'electron'
-
 export default {
-  name: 'Home',
-
-  data () {
-    return {
-      message: `You have ${ remote.process.platform } platform`
-    }
-  }
+  name: 'Home'
 }
 </script>
